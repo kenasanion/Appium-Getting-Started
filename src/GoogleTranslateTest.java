d16c6f00ea426eeccfad18c64e53a398b34c276c
kenasanion <kenasanion@gmail.com>
2018-02-27 03:31:42 +0800

    Improved skeleton code to make the exercise easier

--- src/GoogleTranslateTest.java
+++ src/GoogleTranslateTest.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.net.URL;
 
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -25,15 +27,28 @@ public class GoogleTranslateTest {
 		
 		if (file != null) {
 			// Initialize and set our desire capabilities here
-			
+			DesiredCapabilities dc = new DesiredCapabilities();
+
+			// Do not remove. Will explain in the next session
+			dc.setCapability("appActivity", "com.google.android.apps.translate.TranslateActivity");
+			dc.setCapability("appWaitActivity", "com.google.android.apps.translate.OnboardingActivity");
 			// End of initialization
 			
 			// By default, Adding /wd prefix would cause our webdriver to run with other processes running in this domain
 			URL remoteURL = new URL("http://0.0.0.0:4723/wd/hub");
 			
-			// AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(remoteURL, dc);
+			AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(remoteURL, dc);
+
+			// Here's a bonus, just supply it with ids and xpath
+			// This will handle the setup dialog box.
 			
-			// Add automation code below...
+		    // driver.findElementByXPath("");
+		    // driver.findElementById("").click();
+			
+			// Do steps 2 onwards below...
+			
+			// Uncomment this line after completing your automation
+			// driver.quit();
 		}
  	}
 }
